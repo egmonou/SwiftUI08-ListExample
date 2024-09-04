55d7649d834ec28575bbfeff3b543469b3795124
Nour <mohamednour25@gmail.com>
2024-09-04 12:40:12 +0300

    Initial Commit

---
+++ ListExample/ListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListExampleApp.swift
+//  ListExample
+//
+//  Created by administrator on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
